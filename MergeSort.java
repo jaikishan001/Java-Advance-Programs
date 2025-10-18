e04a7301bf315d41e3711a9e75ad41be678466b6
jaikishan <157561388+jaikishan001@users.noreply.github.com>
2025-10-18 14:38:42 +0530

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,33 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] leftArr = new int[mid - left + 1];
+        int[] rightArr = new int[right - mid];
+
+        for (int i = 0; i < leftArr.length; i++)
+            leftArr[i] = arr[left + i];
+
+        for (int i = 0; i < rightArr.length; i++)
+            rightArr[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) arr[k++] = leftArr[i++];
+            else arr[k++] = rightArr[j++];
+        }
+
+        while (i < leftArr.length) arr[k++] = leftArr[i++];
+        while (j < rightArr.length) arr[k++] = rightArr[j++];
+    }
+}
